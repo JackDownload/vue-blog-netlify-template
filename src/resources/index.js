42a807cd93e2d4c29a7fce443d1677bc50f7d0f9
JackDownload <kismocsy@gmail.com>
2020-08-17 17:33:24 +0200

    Update index.js

--- src/resources/index.js
+++ src/resources/index.js
@@ -1,3 +1,7 @@
 export { default as Blog } from './Blog'
 export { default as BlogFeed } from './BlogFeed'
 export { default as BlogPost } from './BlogPost'
+export { default as RecipeForm } from './RecipeForm'
+export { default as RecipeList } from './RecipeList'
+export { default as RecipeSingle } from './RecipeSingle'
+
